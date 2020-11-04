05583c1389ad0ea00473813f7d48a58b8dd56d36
youngbin <w2000r@naver.com>
2020-11-04 21:20:44 +0900

    add function add_lists

--- anything.py
+++ anything.py
@@ -1,4 +1,5 @@
-# 내용 작성 예시
-if __name__ == "__main__":
-    print("hello python, hi ros.")
+from add_lists import add_lists
 
+if __name__ == "__main__":
+    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
+    print(result)
